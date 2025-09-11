d6143c01a5b17ba64b77d2022d7aea674485cbd5
masriadiNotDev <adiganzxd@gmail.com>
2025-09-11 17:43:02 +0800

    Create day4.java
    sequence di java

---
+++ day4.java
@@ -0,0 +1,14 @@
+public class day4 {
+    public static void main(String[] args) {
+        // kode \t menambah spasi
+        System.out.println("nama :\tAdii");
+        //kode \\ menambah garis miring terbalik pada output
+        System.out.println("contoh : \\");
+        // kode \n teks ke garis baru
+        System.out.println("contohnya  \n hay");
+        //kode \r menghilangkan teks sebelumnya
+        System.out.println("contoh \rhy");
+        
+    }
+    
+}

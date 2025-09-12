2815e67e6301074a5622df7ae1ae3f23417c0842
masriadiNotDev <adiganzxd@gmail.com>
2025-09-12 21:55:36 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,14 @@
+public class Day5 {
+ public static void main(String[] args) {
+   byte angkaByte = 100;
+   short angkaShort = 20000;
+   int angkaInt = 150000;
+   long angkaLong = 10000000000L;
+
+  System.out.println("byte: " + angkaByte);
+  System.out.println("short: " + angkaShort);
+  System.out.println("int: " + angkaInt);
+  System.out.println("long: " + angkaLong);
+ }
+    
+}

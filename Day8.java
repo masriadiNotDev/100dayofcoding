e5baf325983b9a073f65679a0e8e8efe6c0068f3
masriadiNotDev <adiganzxd@gmail.com>
2025-09-15 00:03:50 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,12 @@
+public class Day8 {
+
+    public static void main(String[] args) {
+        String a = "halo"; // DI JAVA HARUS MENGGUNAKAN SIMBOL " " TAK BOLEH ',`, MAKA A4KAN ERROR TIDAK SPEERTI DI BAHASA PY DAN JS
+        String b = "dunia";
+        String gabung = a+b; // DI JAVA BISA MENGGUNAKAN PLUS PADA STRING TAPI OUTPUTNYA AKAN MENGGABUNGKAN NILAI A DAN B TANPA ERROR JIKA SAMA-SAMA STRING
+    System.out.println(gabung); // OUTPUT ==> HALO DUNIA
+////// DI JAVA UNTUK MEMBUAT TIPE DATA HARUS SESUAI AWALAN PENAMAAN VARIABELNYA BERDASARKAN TIPE DATA MISAL : 
+/// String nama_var = "a";
+
+    }
+}

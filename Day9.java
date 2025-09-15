816d54a6e52ba866ade5a840c19eabd652fcb781
masriadiNotDev <adiganzxd@gmail.com>
2025-09-16 03:15:22 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,27 @@
+public class Day9 {
+    public static void main(String[] args) {
+        
+    
+
+    // DEKLARASI ATAU MEMBUAAT VARIABEL KOSONG
+     String nama;
+     int umur;
+
+    // INISIALISASI ATAU MEMBERI NILAI DI VARIABEL KOSONG TADI
+    nama = "adi";
+    umur = 19;
+
+    // CETAK NILAI AWAL 
+    System.out.println("nama : " + nama);
+    System.out.println("umur : " + umur);
+
+
+// UPDATE NILAI VARIABEL 
+  nama = "ambatukammm";
+  umur = 17;
+
+  // NILAI VARIABEL AKAN BERBEDA SETELAH UPDATE TANPA ADA ERROR 
+      System.out.println("nama : " + nama);             
+      System.out.println("umur : " + umur);
+}
+ }

9df53834485ca5e8d0d9b5f0f39a1b46c18e8298
masriadiNotDev <adiganzxd@gmail.com>
2025-09-19 19:11:47 +0800

    Create Day12.java

---
+++ Day12.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Day12 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Masukan nama kamu : ");
+    String nama = scanner.nextLine();
+
+    System.out.print("Umur berapa : ");
+    int umur = scanner.nextInt();
+    scanner.nextLine(); // Clear the buffer after nextInt()
+
+    System.out.print("Domisili mana : ");
+    String alamat = scanner.nextLine();
+
+    System.out.print("Tempat lahir : ");
+    String tl = scanner.nextLine();
+
+    System.out.print("Bulan : ");
+    String bulan = scanner.nextLine();
+
+    System.out.print("Tanggal : ");
+    String tanggal = scanner.nextLine();
+
+    System.out.print("Tahun : ");
+    String tahun = scanner.nextLine();
+
+    System.out.println("------------ [BIODATA] ----------------------");
+    System.out.println("[1] NAMA   : " + nama);
+    System.out.println("[2] ALAMAT : " + alamat);
+    System.out.println("[3] UMUR   : " + umur);
+    System.out.println("[4] TTG    : " + tanggal + " " + bulan + " " + tahun + " di " + tl);
+    System.out.println("---------------------------------------------");
+
+    scanner.close();
+  }
+}
